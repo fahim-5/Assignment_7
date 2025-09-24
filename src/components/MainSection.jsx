cbf052d3df6f7b864ae25f3d9e36d0753043498a
Fahim Faysal <mfaysal223224@bscse.uiu.ac.bd>
2025-09-25 00:15:52 +0600

    all the compnet ready and loading perftly form app

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MainSection extends Component {
+  render() {
+    return (
+      <div>MainSection</div>
+    )
+  }
+}
---
+++ src/components/TicketCard.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TicketCard extends Component {
+  render() {
+    return (
+      <div>TicketCard</div>
+    )
+  }
+}
